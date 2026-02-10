ff67bca8aa43181efa12e0982b5401bf9dfc2906
FerraroLuca <ferraroluca765@gmail.com>
2026-02-10 23:17:57 +0100

    import './index.css
    tolta riga

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // Se hai stili globali aggiuntivi
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
